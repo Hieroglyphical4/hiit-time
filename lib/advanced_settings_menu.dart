ec56a54ebfc8aece260768ba8c3fb378655b113d
OMalley <omalleygab@gmail.com>
2023-01-31 07:52:41 -0500

    Submenus

--- lib/advanced_settings_menu.dart
+++ lib/advanced_settings_menu.dart
@@ -1,9 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
 import 'Config/settings.dart';
 
+// This is the Parent Widget from which other settings menus are opened
 class AdvancedSettingsMenu extends StatefulWidget {
 
   const AdvancedSettingsMenu({
@@ -67,14 +65,17 @@ class _AdvancedSettingsMenuState extends State<AdvancedSettingsMenu> {
                           ? 'Audio Settings'
                           : '- Audio Settings -',
                           style: TextStyle(fontFamily: 'AstroSpace', fontSize: 30, height: 1.1),
-                          textAlign: TextAlign.center))),
+                          textAlign: TextAlign.center
+                      )
+                  )
+                ),
 
                 // Determine if Audio Settings Widget should show:
                 _displayAudioSettings
                   ? AudioSettingsWidget()
                   : Container(),
 
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
 
                 ///////////////////////////
                 // Theme Settings Button
@@ -117,7 +118,7 @@ class _AdvancedSettingsMenuState extends State<AdvancedSettingsMenu> {
 }
 
 ////////////////////////////////////////////
-// Widget for all Theme related Settings
+// Widget for all Theme related Settings (submenu)
 ////////////////////////////////////////////
 class ThemeSettingsWidget extends StatefulWidget {
   const ThemeSettingsWidget({super.key});
@@ -139,7 +140,7 @@ class ThemeSettingsWidgetState extends State<ThemeSettingsWidget> {
 }
 
 ////////////////////////////////////////////
-// Widget for all Audio related Settings
+// Widget for all Audio related Settings (submenu)
 ////////////////////////////////////////////
 class AudioSettingsWidget extends StatefulWidget {
   const AudioSettingsWidget({super.key});
@@ -149,6 +150,101 @@ class AudioSettingsWidget extends StatefulWidget {
 }
 
 class AudioSettingsWidgetState extends State<AudioSettingsWidget> {
+  bool _displayTimerAudioSettings = true;
+  bool _displayButtonAudioSettings = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const SizedBox(height: 20),
+        /////////////////////////
+        // Timer Submenu Button
+        /////////////////////////
+        SizedBox(
+            height: 45,
+            width: 200,
+            child: ElevatedButton(
+                onPressed: () => setState(() {
+                  if (_displayTimerAudioSettings) {
+                    _displayTimerAudioSettings = false;
+                  } else {
+                    _displayTimerAudioSettings = true;
+                  }
+                }),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: secondaryAccentColor,
+                  // shape: Rectangle(),
+                  padding: EdgeInsets.all(4),
+                ),
+                child: Text(_displayTimerAudioSettings
+                    ? 'Timer'
+                    : '- Timer -',
+                    style: TextStyle(fontFamily: 'AstroSpace', fontSize: 25, height: 1.1),
+                    textAlign: TextAlign.center
+                )
+            )
+        ),
+
+        // Determine if Timer Audio Submenu Widget should show:
+        _displayTimerAudioSettings
+            ? TimerAudioSettingsWidget()
+            : Container(),
+
+        const SizedBox(height: 20),
+
+        ////////////////////////////
+        // Buttons Submenu Button
+        ////////////////////////////
+        SizedBox(
+            height: 45,
+            width: 200,
+            child: ElevatedButton(
+                onPressed: () => setState(() {
+                  if (_displayButtonAudioSettings) {
+                    _displayButtonAudioSettings = false;
+                  } else {
+                    _displayButtonAudioSettings = true;
+                  }
+                }),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: secondaryAccentColor,
+                  // shape: Rectangle(),
+                  padding: EdgeInsets.all(4),
+                ),
+                child: Text(_displayButtonAudioSettings
+                    ? 'Buttons'
+                    : '- Buttons -',
+                    style: TextStyle(fontFamily: 'AstroSpace', fontSize: 25, height: 1.1),
+                    textAlign: TextAlign.center
+                )
+            )
+        ),
+
+        // Determine if Timer Audio Submenu Widget should show:
+        _displayButtonAudioSettings
+            ? ButtonAudioSettingsWidget()
+            : Container(),
+
+        const SizedBox(height: 20),
+        SizedBox(height: 1, child: Container(color: Colors.grey)),
+      ],
+    );
+  }
+}
+
+////////////////////////////////////////////////
+// Widget for all Timer Audio related Settings (sub-submenu)
+////////////////////////////////////////////////
+class TimerAudioSettingsWidget extends StatefulWidget {
+  const TimerAudioSettingsWidget({super.key});
+
+  @override
+  TimerAudioSettingsWidgetState createState() => TimerAudioSettingsWidgetState();
+}
+
+class TimerAudioSettingsWidgetState extends State<TimerAudioSettingsWidget> {
   void _onTimerAlarmChanged(bool value) {
     setState(() {
       if (timerAlarmEnabled) {
@@ -179,6 +275,117 @@ class AudioSettingsWidgetState extends State<AudioSettingsWidget> {
     });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(height: 20),
+          /////////////////////////////
+          // Timer Alarm Settings
+          /////////////////////////////
+          Row(
+            children: [
+              const SizedBox(width: 20),
+
+              Text('Timer Alarm',
+                  style: TextStyle(
+                      color: timerAlarmEnabled
+                          ? primaryColor
+                          : Colors.grey,
+                      fontFamily: 'AstroSpace',
+                      fontSize: 20,
+                      height: 1.1),
+                  textAlign: TextAlign.center),
+
+              Spacer(),
+
+              Switch(
+                value: timerAlarmEnabled,
+                onChanged: _onTimerAlarmChanged,
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 20),
+          SizedBox(height: 1, child: Container(color: Colors.grey)),
+          const SizedBox(height: 20),
+
+          /////////////////////////////
+          // 3-2-1 Countdown Settings
+          /////////////////////////////
+          Row(
+            children: [
+              const SizedBox(width: 20),
+
+              Text('3-2-1 Countdown',
+                  style: TextStyle(
+                      color: threeTwoOneCountdownEnabled
+                          ? primaryColor
+                          : Colors.grey,
+                      fontFamily: 'AstroSpace',
+                      fontSize: 20,
+                      height: 1.1),
+                  textAlign: TextAlign.center),
+
+              Spacer(),
+
+              Switch(
+                value: threeTwoOneCountdownEnabled,
+                onChanged: _on321CountdownChanged,
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 20),
+          SizedBox(height: 1, child: Container(color: Colors.grey)),
+          const SizedBox(height: 20),
+          ///////////////////////////////
+          // Mode Switch Audio Settings
+          //////////////////////////////
+          Row(
+            children: [
+              const SizedBox(width: 20),
+
+              Text('Mode Switch Alert',
+                  style: TextStyle(
+                      color: modeSwitchAlertEnabled
+                          ? primaryColor
+                          : Colors.grey,
+                      fontFamily: 'AstroSpace',
+                      fontSize: 20,
+                      height: 1.1),
+                  textAlign: TextAlign.center),
+
+              Spacer(),
+
+              Switch(
+                value: modeSwitchAlertEnabled,
+                onChanged: _onModeSwitchAlertChanged,
+              ),
+
+            ],
+          ),
+
+          const SizedBox(height: 20),
+          SizedBox(height: 1, child: Container(color: Colors.grey)),
+          const SizedBox(height: 20),
+        ]
+    );
+  }
+}
+
+////////////////////////////////////////////////
+// Widget for all Button Audio related Settings (sub-submenu)
+////////////////////////////////////////////////
+class ButtonAudioSettingsWidget extends StatefulWidget {
+  const ButtonAudioSettingsWidget({super.key});
+
+  @override
+  ButtonAudioSettingsWidgetState createState() => ButtonAudioSettingsWidgetState();
+}
+
+class ButtonAudioSettingsWidgetState extends State<ButtonAudioSettingsWidget> {
   void _onSaveButtonAudioChanged(bool value) {
     setState(() {
       if (saveButtonAudioEnabled) {
@@ -222,220 +429,126 @@ class AudioSettingsWidgetState extends State<AudioSettingsWidget> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const SizedBox(height: 20),
-        /////////////////////////////
-        // Timer Alarm Settings
-        /////////////////////////////
-        Row(
-          children: [
-            const SizedBox(width: 20),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(height: 20),
+          ////////////////////////
+          // Restart Button Audio
+          ////////////////////////
+          Row(
+            children: [
+              const SizedBox(width: 20),
 
-            Text('Timer Alarm',
-                style: TextStyle(
-                    color: timerAlarmEnabled
-                        ? primaryColor
-                        : Colors.grey,
-                    fontFamily: 'AstroSpace',
-                    fontSize: 20,
-                    height: 1.1),
-                textAlign: TextAlign.center),
+              Text('Restart Button Audio',
+                  style: TextStyle(
+                      color: restartButtonAudioEnabled
+                          ? primaryColor
+                          : Colors.grey,
+                      fontFamily: 'AstroSpace',
+                      fontSize: 20,
+                      height: 1.1),
+                  textAlign: TextAlign.center),
 
-            Spacer(),
+              Spacer(),
 
-            Switch(
-              value: timerAlarmEnabled,
-              onChanged: _onTimerAlarmChanged,
-            ),
-          ],
-        ),
+              Switch(
+                value: restartButtonAudioEnabled,
+                onChanged: _onRestartButtonAudioChanged,
+              ),
 
-        const SizedBox(height: 20),
-        SizedBox(height: 1, child: Container(color: Colors.grey)),
-        const SizedBox(height: 20),
+            ],
+          ),
 
-        /////////////////////////////
-        // 3-2-1 Countdown Settings
-        /////////////////////////////
-        Row(
-          children: [
-            const SizedBox(width: 20),
+          const SizedBox(height: 20),
+          SizedBox(height: 1, child: Container(color: Colors.grey)),
+          const SizedBox(height: 20),
 
-            Text('3-2-1 Countdown',
-                style: TextStyle(
-                    color: threeTwoOneCountdownEnabled
-                        ? primaryColor
-                        : Colors.grey,
-                    fontFamily: 'AstroSpace',
-                    fontSize: 20,
-                    height: 1.1),
-                textAlign: TextAlign.center),
+          ///////////////////////
+          // Save Button Audio
+          ///////////////////////
+          Row(
+            children: [
+              const SizedBox(width: 20),
+              Text('Save Button Audio',
+                  style: TextStyle(
+                      color: saveButtonAudioEnabled
+                          ? primaryColor
+                          : Colors.grey,
+                      fontFamily: 'AstroSpace',
+                      fontSize: 20,
+                      height: 1.1),
+                  textAlign: TextAlign.center),
 
-            Spacer(),
+              Spacer(),
 
-            Switch(
-              value: threeTwoOneCountdownEnabled,
-              onChanged: _on321CountdownChanged,
-            ),
-          ],
-        ),
+              Switch(
+                value: saveButtonAudioEnabled,
+                onChanged: _onSaveButtonAudioChanged,
+              ),
+            ],
+          ),
 
-        const SizedBox(height: 20),
-        SizedBox(height: 1, child: Container(color: Colors.grey)),
-        const SizedBox(height: 20),
-        ///////////////////////////////
-        // Mode Switch Audio Settings
-        //////////////////////////////
-        Row(
-          children: [
-            const SizedBox(width: 20),
+          const SizedBox(height: 20),
+          SizedBox(height: 1, child: Container(color: Colors.grey)),
+          const SizedBox(height: 20),
 
-            Text('Mode Switch Alert',
-                style: TextStyle(
-                    color: modeSwitchAlertEnabled
-                        ? primaryColor
-                        : Colors.grey,
-                    fontFamily: 'AstroSpace',
-                    fontSize: 20,
-                    height: 1.1),
-                textAlign: TextAlign.center),
+          /////////////////////////
+          // Cancel Button Audio
+          /////////////////////////
+          Row(
+            children: [
+              const SizedBox(width: 20),
 
-            Spacer(),
+              Text('Cancel Button Audio',
+                  style: TextStyle(
+                      color: cancelButtonAudioEnabled
+                          ? primaryColor
+                          : Colors.grey,
+                      fontFamily: 'AstroSpace',
+                      fontSize: 20,
+                      height: 1.1),
+                  textAlign: TextAlign.center),
 
-            Switch(
-              value: modeSwitchAlertEnabled,
-              onChanged: _onModeSwitchAlertChanged,
-            ),
+              Spacer(),
 
-          ],
-        ),
+              Switch(
+                value: cancelButtonAudioEnabled,
+                onChanged: _onCancelButtonAudioChanged,
+              ),
 
-        const SizedBox(height: 20),
-        SizedBox(height: 1, child: Container(color: Colors.grey)),
-        const SizedBox(height: 20),
+            ],
+          ),
 
-        ////////////////////////
-        // Restart Button Audio
-        ////////////////////////
-        Row(
-          children: [
-            const SizedBox(width: 20),
+          const SizedBox(height: 20),
+          SizedBox(height: 1, child: Container(color: Colors.grey)),
+          const SizedBox(height: 20),
 
-            Text('Restart Button Audio',
-                style: TextStyle(
-                    color: restartButtonAudioEnabled
-                        ? primaryColor
-                        : Colors.grey,
-                    fontFamily: 'AstroSpace',
-                    fontSize: 20,
-                    height: 1.1),
-                textAlign: TextAlign.center),
+          /////////////////////////
+          // Switch Button Audio
+          /////////////////////////
+          Row(
+            children: [
+              const SizedBox(width: 20),
 
-            Spacer(),
+              Text('Switch Button Audio',
+                  style: TextStyle(
+                      color: switchButtonAudioEnabled
+                          ? primaryColor
+                          : Colors.grey,
+                      fontFamily: 'AstroSpace',
+                      fontSize: 20,
+                      height: 1.1),
+                  textAlign: TextAlign.center),
 
-            Switch(
-              value: restartButtonAudioEnabled,
-              onChanged: _onRestartButtonAudioChanged,
-            ),
+              Spacer(),
 
-          ],
-        ),
-
-        const SizedBox(height: 20),
-        SizedBox(height: 1, child: Container(color: Colors.grey)),
-        const SizedBox(height: 20),
-
-        ///////////////////////
-        // Save Button Audio
-        ///////////////////////
-        Row(
-          children: [
-            const SizedBox(width: 20),
-            Text('Save Button Audio',
-                style: TextStyle(
-                    color: saveButtonAudioEnabled
-                        ? primaryColor
-                        : Colors.grey,
-                    fontFamily: 'AstroSpace',
-                    fontSize: 20,
-                    height: 1.1),
-                textAlign: TextAlign.center),
-
-            Spacer(),
-
-            Switch(
-              value: saveButtonAudioEnabled,
-              onChanged: _onSaveButtonAudioChanged,
-            ),
-          ],
-        ),
-
-        const SizedBox(height: 20),
-        SizedBox(height: 1, child: Container(color: Colors.grey)),
-        const SizedBox(height: 20),
-
-        /////////////////////////
-        // Cancel Button Audio
-        /////////////////////////
-        Row(
-          children: [
-            const SizedBox(width: 20),
-
-            Text('Cancel Button Audio',
-                style: TextStyle(
-                    color: cancelButtonAudioEnabled
-                        ? primaryColor
-                        : Colors.grey,
-                    fontFamily: 'AstroSpace',
-                    fontSize: 20,
-                    height: 1.1),
-                textAlign: TextAlign.center),
-
-            Spacer(),
-
-            Switch(
-              value: cancelButtonAudioEnabled,
-              onChanged: _onCancelButtonAudioChanged,
-            ),
-
-          ],
-        ),
-
-        const SizedBox(height: 20),
-        SizedBox(height: 1, child: Container(color: Colors.grey)),
-        const SizedBox(height: 20),
-
-        /////////////////////////
-        // Switch Button Audio
-        /////////////////////////
-        Row(
-          children: [
-            const SizedBox(width: 20),
-
-            Text('Switch Button Audio',
-                style: TextStyle(
-                    color: switchButtonAudioEnabled
-                        ? primaryColor
-                        : Colors.grey,
-                    fontFamily: 'AstroSpace',
-                    fontSize: 20,
-                    height: 1.1),
-                textAlign: TextAlign.center),
-
-            Spacer(),
-
-            Switch(
-              value: switchButtonAudioEnabled,
-              onChanged: _onSwitchButtonAudioChanged,
-            ),
-          ],
-        ),
-
-        const SizedBox(height: 20),
-        SizedBox(height: 1, child: Container(color: Colors.grey)),
-        const SizedBox(height: 20),
-      ],
+              Switch(
+                value: switchButtonAudioEnabled,
+                onChanged: _onSwitchButtonAudioChanged,
+              ),
+            ],
+          ),
+        ]
     );
   }
 }
